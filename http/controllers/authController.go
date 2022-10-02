fb68caaa99d68ea07631e8a7f209623f9f6a6453
Kevin Liao <q85292542000@gmail.com>
2022-10-03 00:30:02 +0800

    feat: add the login or signin logic

--- http/controllers/authController.go
+++ http/controllers/authController.go
@@ -1,12 +1,15 @@
 package controllers
 
 import (
+	"app/models"
 	"context"
+	"fmt"
+	"net/http"
+
 	"github.com/gin-contrib/sessions"
 	"github.com/gin-gonic/gin"
 	"google.golang.org/api/idtoken"
 	"google.golang.org/api/option"
-	"net/http"
 )
 
 func Login(id string) gin.HandlerFunc {
@@ -28,9 +31,28 @@ func Login(id string) gin.HandlerFunc {
 			return
 		}
 
-		
+		sub := fmt.Sprintf("%v", payload.Claims["sub"])
+		email := fmt.Sprintf("%v", payload.Claims["email"])
+		name := fmt.Sprintf("%v", payload.Claims["name"])
+
 		// Check if database have this user's credential.
+		var user models.User
+		models.GetUserByGoogleId(&user, sub)
+
 		// If not, sign up for this user
+		if user.ID == 0 {
+
+			err := models.CreateAUser(&models.User{
+				GoogleId:    sub,
+				Email:       email,
+				DisplayName: name,
+			})
+
+			if err != nil {
+				c.JSON(http.StatusInternalServerError, map[string]string{"status": "auth failed"})
+				return
+			}
+		}
 
 		if session.Get("id") == nil {
 			session.Set("name", payload.Claims["given_name"])
