4c940e54638f49a963f08e3900a3444cf35658c6
Kevin Liao <q85292542000@gmail.com>
2023-07-01 05:32:26 +0800

    fix: Typo

--- http/middlewares/logger.go
+++ http/middlewares/logger.go
@@ -19,7 +19,7 @@ func LoggerMiddleWare(c *gin.Context) {
 		"body_size":     c.Writer.Size(),
 		"client_ip":     c.ClientIP(),
 		"error_message": c.Errors.ByType(gin.ErrorTypePrivate).String(),
-		"lantency":      fmt.Sprintf("%d%s", latency, "µs"),
+		"latency":      fmt.Sprintf("%d%s", latency, "µs"),
 		"method":        c.Request.Method,
 		"path":          c.Request.URL.Path,
 		"status":        c.Writer.Status(),
