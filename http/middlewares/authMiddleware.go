0cb871d55c32207e3f610b15e18a2b2053899fbb
Kevin Liao <q85292542000@gmail.com>
2024-09-07 12:08:48 +0800

    🔊 Add logs at auth middlewrare

--- http/middlewares/authMiddleware.go
+++ http/middlewares/authMiddleware.go
@@ -5,12 +5,16 @@ import (
 
 	"github.com/gin-contrib/sessions"
 	"github.com/gin-gonic/gin"
+	log "github.com/sirupsen/logrus"
 )
 
 func AuthRequired(c *gin.Context) {
 	session := sessions.Default(c)
 	if session.Get("id") == nil {
+		log.Info("Unauthorized access")
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "unauthorized"})
 		c.AbortWithStatus(http.StatusUnauthorized)
+		return
 	}
 
 	c.Next()
