2c41f46f6b66689de82b7e0b3587fbd1b024fa0e
Kevin Liao <q85292542000@gmail.com>
2026-01-25 05:59:25 +0800

    🎨 Rename whiteboard_canvas_data to canvas_data and add tests
    Signed-off-by: Kevin Liao <q85292542000@gmail.com>

---
+++ internal/models/canvas_data_test.go
@@ -0,0 +1,41 @@
+package models
+
+import (
+	"testing"
+
+	"github.com/kevinliao852/e-whiteboard-server/internal/database"
+	"github.com/stretchr/testify/assert"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
+func setupCanvasDataTestDB(t *testing.T) {
+	db, err := gorm.Open(sqlite.Open(":memory:"), &gorm.Config{})
+	assert.NoError(t, err)
+
+	database.DB = db
+
+	err = database.DB.AutoMigrate(&WhiteboardCanvasData{})
+	assert.NoError(t, err)
+}
+
+func TestCreate(t *testing.T) {
+	setupCanvasDataTestDB(t)
+
+	wb := &WhiteboardCanvasData{
+		WhiteboardId: 1,
+		StartX:       10,
+		EndX:         20,
+		StartY:       30,
+		EndY:         40,
+	}
+
+	err := Create(wb)
+	assert.NoError(t, err)
+	assert.NotZero(t, wb.ID)
+	assert.Equal(t, uint(1), wb.WhiteboardId)
+	assert.Equal(t, uint(10), wb.StartX)
+	assert.Equal(t, uint(20), wb.EndX)
+	assert.Equal(t, uint(30), wb.StartY)
+	assert.Equal(t, uint(40), wb.EndY)
+}
